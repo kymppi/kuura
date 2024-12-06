744bf673ba4a772cbd640de3924a1d788318e5e0
Midka <me@midka.dev>
2024-12-06 22:58:52 +0000

    feat: git sha & branch

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log/slog"
 	"os"
 
@@ -10,18 +11,29 @@ import (
 
 var (
 	rootCmd *cobra.Command
-	logger  *slog.Logger
+
+	// Info
+	GitSHA string
+	Branch string
 
 	// Global flags
 	debugMode bool
 )
 
 func main() {
+	if GitSHA == "" {
+		GitSHA = "unknown"
+	}
+	if Branch == "" {
+		Branch = "unknown"
+	}
+
 	rootCmd = &cobra.Command{
-		Use:   "kuura",
-		Short: "Kuura Authentication Server",
-		Long:  `Kuura is an authentication server with great M2M support.`,
-		Run:   kuura.RootCommand,
+		Use:     "kuura",
+		Short:   "Kuura Authentication Server",
+		Long:    `Kuura is an authentication server with great M2M support.`,
+		Run:     kuura.RootCommand,
+		Version: formatVersion(GitSHA, Branch),
 	}
 
 	rootCmd.PersistentFlags().BoolVar(
@@ -35,11 +47,15 @@ func main() {
 
 	logger := kuura.ProvideLogger()
 
-	rootCmd.AddCommand(kuura.VersionCommand())
+	// rootCmd.AddCommand(kuura.VersionCommand())
 	rootCmd.AddCommand(kuura.MigrateCommand(logger))
 
 	if err := rootCmd.Execute(); err != nil {
 		logger.Error("Command execution failed", slog.String("error", err.Error()))
 		os.Exit(1)
 	}
+}
+
+func formatVersion(sha, branch string) string {
+	return fmt.Sprintf("%s (%s)", branch, sha)
 }
