45c5ef3eaf20cd1e835786d56c9ed684f3df6f9b
Midka <me@midka.dev>
2024-12-14 20:59:49 +0000

    feat(commands): jwk private export

--- cmd/jwks.go
+++ cmd/jwks.go
@@ -2,7 +2,12 @@ package cmd
 
 import (
 	"context"
+	"crypto/ecdsa"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
 	"log/slog"
+	"os"
 
 	"github.com/google/uuid"
 	kuura "github.com/kymppi/kuura/internal"
@@ -17,6 +22,7 @@ func runJwks(logger *slog.Logger, config *kuura.Config) *cobra.Command {
 	}
 
 	jwksCmd.AddCommand(jwkCreate(logger, config))
+	jwksCmd.AddCommand(jwkExport(logger, config))
 
 	return jwksCmd
 }
@@ -57,4 +63,70 @@ func jwkCreate(logger *slog.Logger, config *kuura.Config) *cobra.Command {
 			cmd.Printf("New key created with ID: %s for service %s\n", keyID, serviceId)
 		},
 	}
+}
+
+func jwkExport(logger *slog.Logger, config *kuura.Config) *cobra.Command {
+	return &cobra.Command{
+		Use:   "export [service-id] [key-id]",
+		Short: "Export the ECDSA private key for a service",
+		Args:  cobra.ExactArgs(2),
+		Run: func(cmd *cobra.Command, args []string) {
+			ctx := context.Background()
+
+			serviceId, err := uuid.Parse(args[0])
+			if err != nil {
+				cmd.PrintErrf("Failed to parse serviceId: %s", err)
+				return
+			}
+
+			queries, cleanup, err := kuura.InitializeDatabaseConnection(ctx, logger, config)
+			if err != nil {
+				cmd.PrintErrf("Failed to initialize database: %s", err)
+				return
+			}
+			defer cleanup()
+
+			jwkManager, err := kuura.InitializeJWKManager(ctx, logger, config, queries)
+			if err != nil {
+				cmd.PrintErrf("Failed to initialize jwk manager: %s", err)
+				return
+			}
+
+			privateJWK, err := jwkManager.Export(ctx, serviceId, args[1])
+			if err != nil {
+				cmd.PrintErrf("Failed to retrieve private key for service ID %s: %v\n", serviceId.String(), err)
+				return
+			}
+
+			var privateKey ecdsa.PrivateKey
+			err = privateJWK.Raw(&privateKey)
+			if err != nil {
+				cmd.PrintErrf("Failed to get private key from JWK: %v\n", err)
+				return
+			}
+
+			err = exportECDSAPrivateKeyToStdout(&privateKey)
+			if err != nil {
+				cmd.PrintErrf("Failed to export private key to PEM: %v", err)
+				return
+			}
+		},
+	}
+}
+
+func exportECDSAPrivateKeyToStdout(privateKey *ecdsa.PrivateKey) error {
+	privBytes, err := x509.MarshalECPrivateKey(privateKey)
+	if err != nil {
+		return fmt.Errorf("failed to marshal private key: %v", err)
+	}
+
+	err = pem.Encode(os.Stdout, &pem.Block{
+		Type:  "EC PRIVATE KEY",
+		Bytes: privBytes,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to encode private key in PEM format: %v", err)
+	}
+
+	return nil
 }
